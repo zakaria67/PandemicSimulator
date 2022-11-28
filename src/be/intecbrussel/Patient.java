46bba5aaebcc48e24df562687ed40c5735476476
zakaria67 <salsabile-mp@hotmail.com>
2022-11-28 11:51:58 +0100

    init

--- src/be/intecbrussel/Patient.java
+++ src/be/intecbrussel/Patient.java
@@ -1,2 +1,69 @@
-package be.intecbrussel;public class Patient {
+package be.intecbrussel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient {
+    String nationalRegistryNumber;
+    String fullNAme;
+    int age;
+    int temperature;
+    boolean unKnownVirus;
+    boolean ensured;
+
+    public Patient(String nationalRegistryNumber, String fullNAme, int age, int temperature, boolean unKnownVirus, boolean ensured) {
+        this.nationalRegistryNumber = nationalRegistryNumber;
+        this.fullNAme = fullNAme;
+        this.age = age;
+        this.temperature = temperature;
+        this.unKnownVirus = unKnownVirus;
+        this.ensured = ensured;
+    }
+
+    public static List <Patient> getAllPatients(){
+        List <Patient> allPatients = new ArrayList<>();
+        Patient donaldPutin = new Patient("110201321", "Donald Putin", 90, 39, false, true);
+        allPatients.add(new Patient("030201321", "Lucky Luke", 69, 38, true, false));
+        allPatients.add(new Patient("030201322", "Stromae Papaoutai", 35, 40, false, true));
+        allPatients.add(new Patient("030801321", "Gringoire Lune", 80, 36, true, true));
+        allPatients.add(new Patient("687201321", "Crazy Frog", 5, 39, false, false));
+        allPatients.add(new Patient("030244321", "Sanic the HedgeHog", 16, 37, true, true));
+        allPatients.add(new Patient("110201321", "Donald Putin", 90, 39, false, true));
+        allPatients.add(new Patient("110201321", "Donald Putin", 90, 39, false, true));
+        allPatients.add(new Patient("088201321", "Bilbo Baggins", 23, 39, true, false));
+        allPatients.add(new Patient("035501321", "Lucio Correa Dos Santos", 40, 38, false, true));
+        allPatients.add(new Patient("030206621", "Doctor Who", 33, 35, true, true));
+        allPatients.add(new Patient("030245321", "Luke Cage", 46, 41, true, false));
+        allPatients.add(new Patient("030298321", "Naruto Uzumaki", 13, 38, false, false));
+        allPatients.add(new Patient("125201321", "Billy Batson", 15, 40, true, false));
+        allPatients.add(new Patient("987201321", "Joseph Joestar", 75, 36, true, false));
+        allPatients.add(new Patient("645201321", "Jon Snow", 33, 35, false, false));
+        allPatients.add(new Patient("368201321", "Prince Charming", 95, 37, true, true));
+        allPatients.add(new Patient("582201321", "Alexander Hamilton", 65, 38, false, false));
+        allPatients.add(new Patient("687201321", "Crazy Frog", 5, 39, false, false));
+        allPatients.add(new Patient("030201231", "Zelda of Hyrule", 17, 38, false, true));
+        allPatients.add(new Patient("830208651", "Mother Theresa", 98, 39, true, false));
+        allPatients.add(new Patient("030209741", "Willy Wonka", 70, 40, true, true));
+        allPatients.add(new Patient("030205411", "Scrooge McDuck", 73, 38, true, false));
+        allPatients.add(new Patient("368201321", "Prince Charming", 95, 37, true, true));
+        allPatients.add(new Patient("030209861", "Sonia Magnlia", 35, 39, false, false));
+        allPatients.add(new Patient("030278921", "Peter Parker", 21, 40, true, false));
+        allPatients.add(new Patient("030269821", "Bruce Wayne", 46, 35, false, true));
+        allPatients.add(new Patient("030215421", "Seto Kaiba", 22, 38, false, true));
+        allPatients.add(new Patient("030296321", "Mario Mario", 50, 37, false, false));
+        allPatients.add(new Patient("035051321", "Harry Potter", 18, 39, true, true));
+        allPatients.add(new Patient("368201321", "Prince Charming", 95, 37, true, true));
+        allPatients.add(new Patient("036781321", "Mary Poppins", 38, 38, true, false));
+        allPatients.add(new Patient("033691321", "Alexander the Great", 51, 37, false, true));
+        allPatients.add(new Patient("687201321", "Crazy Frog", 5, 39, false, false));
+        allPatients.add(new Patient("030201000", "Huey Freeman", 12, 40, false, false));
+        allPatients.add(new Patient("000201321", "Miku Hatsune", 16, 36, true, true));
+        allPatients.add(new Patient("111201321", "Drake Josh", 33, 38, true, false));
+        allPatients.add(new Patient("222201321", "April O'Neil", 29, 37, false, false));
+        return allPatients;
+    }
+
+
+
+
 }
